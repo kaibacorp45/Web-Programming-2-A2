2b8f849cec3cd60273e10e96ab2c807068b9bfe9
kaibacorp45 <kaibacorp45@gmail.com>
2022-03-11 17:44:25 +0000

    First commit

--- App/main.py
+++ App/main.py
@@ -4,7 +4,8 @@ from flask_jwt import JWT, jwt_required, current_identity
 from sqlalchemy.exc import IntegrityError
 from datetime import timedelta 
 
-from models import db #, User, Pokemon, MyPokemon
+from models import db, User, Pokemon, MyPokemon
+
 
 ''' Begin boilerplate code '''
 def create_app():
@@ -19,17 +20,48 @@ def create_app():
 app = create_app()
 
 app.app_context().push()
-
 ''' End Boilerplate Code '''
 
 ''' Set up JWT here '''
+def authenticate(uname, password):
+  #search for the specified user
+  user = User.query.filter_by(username=uname).first()
+  #if user is found and password matches
+  if user and user.check_password(password):
+    return user
 
+#Payload is a dictionary which is passed to the function by Flask JWT
+def identity(payload):
+  return User.query.get(payload['identity'])
+
+jwt = JWT(app, authenticate, identity)
 ''' End JWT Setup '''
 
 # edit to query 50 pokemon objects and send to template
+
 @app.route('/')
 def index():
-  return render_template('index.html')
+  poke = Pokemon.query.filter_by
+  return render_template('index.html') 
+
+@app.route('/pokemon', methods=['GET'])
+def list_pokemon():
+  userdata = request.get_json()
+
+  olduser = User.query.filter_by(username=userdata['username']).first()
+
+  if not olduser:
+    olduser = User.query.filter_by(email=userdata['email']).first()
+
+  if olduser:
+    return 'username or email already exists'
+
+  else:
+    newuser = User(username=userdata['username'], email=userdata['email'])
+    newuser.set_password(userdata['password'])
+    db.session.add(newuser)
+    db.session.commit()
+  return 'user created'
 
 @app.route('/app')
 def client_app():
